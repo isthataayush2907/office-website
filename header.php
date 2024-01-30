eb628e76000c4894030a8be106f92fb2b2dc40f3
isthataayush2907 <aayushsharma2907@gmail.com>
2024-01-30 15:47:21 +0530

    header and footer file add with css folder

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand me-5 fw-bold fs-3 " href="#">AAYUSH</a>
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item fw-bold ">
+                        <a class="nav-link  me-2" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="rooms.php">Study</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="facility.php">Tourist Visa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="facility.php">Spouse Visa</a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="contact-us.php"> Contact Us</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+
+                    <button type="button" class="btn btn-outline-dark rounded-pill shadow-none me-lg-3 me-2 " data-bs-toggle="modal" data-bs-target="#loginModal">
+                    Contact Us
+                    </button>
+
+                </form>
+            </div>
+        </div>
+    </nav>
